72bd9c58d427df9a8ab8ea78f5f56b9be5683242
delfi26 <delfinacano3004@gmail.com>
2024-10-23 19:25:56 -0300

    Estos archivos pertenecen a mi carpeta persona.
    Son los trabajos realizados durante la clase 1.

---
+++ delfina/Clase 1/saludo_personalizado.py
@@ -0,0 +1,3 @@
+# utilizamos INPUT (es la forma por la que ingreso datos a la consola y me los devuelve como strin)
+nombre=input("Decime tu nombre: ")
+print("Hola "+nombre)
---
+++ delfina/Clase 1/sumadora.py
@@ -0,0 +1,10 @@
+#realizamos calculadora que solo suma.
+#utilizamos el int para convertir el dato infresado en entero
+#le pedimos al usuario que ingrese un numero por consola
+a=int(input("Ingresa un dato: "))
+#le pedimos al usuario que infrese otro dato
+b=int(input("Ingrese otro dato: "))
+#realizamos la suma
+c=a+b
+#le mostramos el resultado al usuario
+print("El resultado de la suma es: "+str(c))
