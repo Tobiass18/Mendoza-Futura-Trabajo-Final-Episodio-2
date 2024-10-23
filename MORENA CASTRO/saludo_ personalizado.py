783bb7b8f5a45f28f547374dd416d1dc3d9192f0
morena2897 <morec2897@gmail.com>
2024-10-23 19:33:50 -0300

    Carpeta personal More castro
    Actividades clase 1

---
+++ MORENA CASTRO/saludo_ personalizado.py
@@ -0,0 +1,6 @@
+# ingresamos un dato 
+#dato=input ("ingresa tu nombre")
+#print("hola " + dato)
+dato=int(input ("ingresa un numero: "))
+dato2=int(input("Ingrese otro numero: "))
+print(dato+dato2)
