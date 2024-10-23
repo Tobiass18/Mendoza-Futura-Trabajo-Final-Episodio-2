17aabeab61ec15c68d82ced39514a42e8528201e
maximo0914 <maxicoleccionista@gmail.com>
2024-10-23 19:31:22 -0300

    carpeta personal Maximo Salinas

---
+++ maximo salinas/clase 1/saludo_personalizado.py
@@ -0,0 +1,8 @@
+# Utilizamos el IMPUT : Es la forma en la que yo ingreso datos por la consola 
+# y me los devuelve como un string
+
+nombre = input("decime tu nombre: ")
+print("Hola " + nombre)
+
+
+
---
+++ maximo salinas/clase 1/sumadora.py
@@ -0,0 +1,11 @@
+# Realizamos una calculadora que solo suma:
+
+# utilizamos el int para convertir el dato ingresado a entero
+# Le pedimos al usuario que ingrese un numero por consola
+a = int(input("ingrsa un numero: "))
+# Le pedimos al ususario que ingrese otro numero
+b = int("ingrese otro numero: ")
+# Realizamos la suma 
+c = a + b
+# Le mostramos el resultado al usuario
+print("el resultado de la suma es: " +str(c))
